e5507d7ade88a6fc8740d29edd3b5818d9f2401f
Alyona Starkova <alyona.starrkova@yandex.ru>
2023-12-29 00:54:53 +0500

    add new function to str_func.py and docstring

--- str_func.py
+++ str_func.py
@@ -1,2 +1,12 @@
 def make_title_all(value):
+    """
+    Принимает на вход строку и возвращает ее со всеми заглавными буквами.
+    """
     return value.upper()
+
+
+def make_title_first_letters(value):
+    """
+    Принимает на вход строку и делает заглавными первые буквы
+    """
+    return value.title()
